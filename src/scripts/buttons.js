77636ae42e07bd9e18443550afd1b477b3d348ec
Samuel Heid <sjheid@t-online.de>
2024-06-03 12:31:09 +0200

    chore: refactoring with new rules

--- src/scripts/buttons.js
+++ src/scripts/buttons.js
@@ -1,28 +1,28 @@
-const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu");
-const line1 = document.getElementById("line1");
-const line2 = document.getElementById("line2");
-const line3 = document.getElementById("line3");
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
 
 let menuOpen = false;
-btn.addEventListener("click", function () {
-  menu.classList.toggle("opacity-100");
-  line1.classList.toggle("-rotate-45");
-  line1.classList.toggle("translate-y-2");
-  line3.classList.toggle("rotate-45");
-  line2.classList.toggle("-translate-x-4");
+btn.addEventListener('click', function () {
+  menu.classList.toggle('opacity-100');
+  line1.classList.toggle('-rotate-45');
+  line1.classList.toggle('translate-y-2');
+  line3.classList.toggle('rotate-45');
+  line2.classList.toggle('-translate-x-4');
 
   if (menuOpen) {
     // add classes if menu is closed
-    line3.classList.add("translate-y-4");
-    line3.classList.add("w-5");
-    line3.classList.remove("w-6");
+    line3.classList.add('translate-y-4');
+    line3.classList.add('w-5');
+    line3.classList.remove('w-6');
     menuOpen = false;
   } else {
     // add classes if menu is opend
-    line3.classList.remove("translate-y-4");
-    line3.classList.add("translate-y-2");
-    line3.classList.add("w-6");
+    line3.classList.remove('translate-y-4');
+    line3.classList.add('translate-y-2');
+    line3.classList.add('w-6');
 
     menuOpen = true;
   }
--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,24 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   /**
    * Function to change the color of the navbar when scrolling
    */
-    window.addEventListener("scroll", function () {
-    const colorDiv = document.getElementById("colorDiv");
-    const navDiv = document.getElementById("navColor");
+  window.addEventListener('scroll', function () {
+    const colorDiv = document.getElementById('colorDiv');
+    const navDiv = document.getElementById('navColor');
+
     if (window.scrollY > 50) {
-      colorDiv.classList.remove("bg-transparent");
-      colorDiv.classList.add("md:bg-zinc-900");
-      colorDiv.classList.add("backdrop-blur-lg");
-      colorDiv.classList.add("bg-black/10");
-      navDiv.classList.remove("text-zinc-900"); // text color of nav text items
-      navDiv.classList.add("text-white");
+      colorDiv.classList.remove('bg-transparent');
+      colorDiv.classList.add('md:bg-zinc-900');
+      colorDiv.classList.add('backdrop-blur-lg');
+      colorDiv.classList.add('bg-black/10');
+      navDiv.classList.remove('text-zinc-900'); // text color of nav text items
+      navDiv.classList.add('text-white');
     } else {
-      colorDiv.classList.remove("md:bg-zinc-900");
-      colorDiv.classList.add("bg-transparent");
-      colorDiv.classList.remove("backdrop-blur-lg");
-      colorDiv.classList.remove("bg-black/10");
-      navDiv.classList.remove("text-white");
-      navDiv.classList.add("text-zinc-900");
+      colorDiv.classList.remove('md:bg-zinc-900');
+      colorDiv.classList.add('bg-transparent');
+      colorDiv.classList.remove('backdrop-blur-lg');
+      colorDiv.classList.remove('bg-black/10');
+      navDiv.classList.remove('text-white');
+      navDiv.classList.add('text-zinc-900');
     }
   });
 });

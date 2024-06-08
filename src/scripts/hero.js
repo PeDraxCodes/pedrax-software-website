e42b4677691af5da71c8b55730716238b73b611c
Samuel Heid <sjheid@t-online.de>
2024-06-08 17:40:03 +0200

    feat(footer): add footer

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,24 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const more_info_btn = document.getElementById('moreInfo');
 
-document.addEventListener("DOMContentLoaded", function() {
-    const more_info_btn = document.getElementById('moreInfo');
-  
-    
-    more_info_btn.addEventListener("mouseenter", function() {
-        console.log("hovered");
-        gsap.to(more_info_btn, { 
-            duration: 0.5, 
-            scale: 1.2,
-          ease: "bounce.out" 
-        });
-      });
-
-
-      more_info_btn.addEventListener("mouseleave", function() {
-      gsap.to(more_info_btn, { 
-        duration: 0.5, 
-        scale: 1, 
-        ease: "bounce.out" 
-      });
+  more_info_btn.addEventListener('mouseenter', function () {
+    gsap.to(more_info_btn, {
+      duration: 0.5,
+      scale: 1.1,
+      ease: 'back.out',
     });
   });
-  
+
+  more_info_btn.addEventListener('mouseleave', function () {
+    gsap.to(more_info_btn, {
+      duration: 0.5,
+      scale: 1,
+      ease: 'back.out',
+    });
+  });
+});

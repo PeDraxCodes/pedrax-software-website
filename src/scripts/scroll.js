f41cd465a8b6f17161240d3d2d793ce5d2ea5a19
Sven Reepschläger <sven.reep@gmail.com>
2024-06-26 15:55:33 +0200

    feat(scroll.js): add function to prevent hrefs to anchors from changing the URL

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -8,21 +8,6 @@ window.addEventListener('scroll', () => {
   const navContactButton = document.getElementById('navContact');
   const pageScrollButton = document.getElementById('pageScrollButton');
 
-    if (window.scrollY > 50) {
-      colorDiv.classList.remove('bg-transparent');
-      colorDiv.classList.add('md:bg-zinc-900');
-      colorDiv.classList.add('backdrop-blur-lg');
-      colorDiv.classList.add('bg-black/10');
-      navDiv.classList.remove('text-zinc-900'); // text color of nav text items
-      navDiv.classList.add('text-white');
-    } else {
-      colorDiv.classList.remove('md:bg-zinc-900');
-      colorDiv.classList.add('bg-transparent');
-      colorDiv.classList.remove('backdrop-blur-lg');
-      colorDiv.classList.remove('bg-black/10');
-      navDiv.classList.remove('text-white');
-      navDiv.classList.add('text-zinc-900');
-    }
   if (window.scrollY > 50) {
     header.classList.remove('bg-transparent');
     header.classList.add('md:bg-zinc-900');
@@ -46,5 +31,15 @@ window.addEventListener('scroll', () => {
   }
 });
 
+// Source: https://stackoverflow.com/a/78248142
+// Smooth scroll to anchor links without changing the URL
+const aWithHref = document.querySelectorAll('a[href*="#"]');
+aWithHref.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (el.hash)
+      window.scrollTo({
+        top: document.querySelector(el.hash).offsetTop,
+      });
   });
 });

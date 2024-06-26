d27db1d595cecfe7aaf1d387fbee878957609a84
Sven Reepschläger <sven.reep@gmail.com>
2024-06-26 15:54:49 +0200

    feat(scroll.js): add pageScrollButton logic + refactoring

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,10 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  /**
-   * Function to change the color of the navbar when scrolling
-   */
-  window.addEventListener('scroll', () => {
-    const colorDiv = document.getElementById('colorDiv');
-    const navDiv = document.getElementById('navColor');
+/**
+ * Function to change the color of the navbar when scrolling
+ */
+window.addEventListener('scroll', () => {
+  const header = document.getElementById('header');
+  const navLogo = document.getElementById('navLogo');
+  const navItemsDesktop = document.getElementById('navItemsDesktop');
+  const navContactButton = document.getElementById('navContact');
+  const pageScrollButton = document.getElementById('pageScrollButton');
 
     if (window.scrollY > 50) {
       colorDiv.classList.remove('bg-transparent');
@@ -21,5 +23,28 @@ document.addEventListener('DOMContentLoaded', () => {
       navDiv.classList.remove('text-white');
       navDiv.classList.add('text-zinc-900');
     }
+  if (window.scrollY > 50) {
+    header.classList.remove('bg-transparent');
+    header.classList.add('md:bg-zinc-900');
+    header.classList.add('backdrop-blur-lg');
+    header.classList.add('bg-black/10');
+    navLogo.classList.replace('text-green-700', 'text-white'); // text color of nav logo
+    navItemsDesktop.classList.replace('text-zinc-900', 'text-white'); // text color of nav text items
+    navContactButton.classList.replace('text-zinc-900', 'text-white');
+    pageScrollButton.classList.replace('opacity-100', 'opacity-0');
+    pageScrollButton.classList.replace('visible', 'invisible');
+  } else {
+    header.classList.remove('md:bg-zinc-900');
+    header.classList.add('bg-transparent');
+    header.classList.remove('backdrop-blur-lg');
+    header.classList.remove('bg-black/10');
+    navLogo.classList.replace('text-white', 'text-green-700'); // text color of nav logo
+    navItemsDesktop.classList.replace('text-white', 'text-zinc-900');
+    navContactButton.classList.replace('text-white', 'text-zinc-900');
+    pageScrollButton.classList.replace('invisible', 'visible');
+    pageScrollButton.classList.replace('opacity-0', 'opacity-100');
+  }
+});
+
   });
 });
